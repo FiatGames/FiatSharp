b8ea695fae6dfad2dca7b783c5587b9aed4cdb26
BELLARMINE\aruf <aruf@bellarmine.edu>
2018-05-02 14:04:18 -0400

    marked actions virtual

--- FiatSharp/Controllers/FiatGameController.cs
+++ FiatSharp/Controllers/FiatGameController.cs
@@ -8,43 +8,43 @@ namespace FiatSharp.Controllers
         public abstract IFiatGame<Settings, State, Move, ClientSettings, ClientState> GetFiatGame();
 
         [Route("~/defaultSettings")]
-        public Settings GetDefaultSettings()
+        public virtual Settings GetDefaultSettings()
         {
             return GetFiatGame().DefaultSettings();
         }
 
         [Route("~/addPlayer")]
-        public Settings PostAddPlayer([FromBody] AddPlayerRequest<Settings> req)
+        public virtual Settings PostAddPlayer([FromBody] AddPlayerRequest<Settings> req)
         {
             return GetFiatGame().AddPlayer(req.player, req.settings);
         }
 
         [Route("~/initialGameState")]
-        public InitialGameStateResult<Settings,State,Move> PostInitialGameState([FromBody] Settings settings)
+        public virtual InitialGameStateResult<Settings,State,Move> PostInitialGameState([FromBody] Settings settings)
         {
             return GetFiatGame().InitialGameState(settings);
         }
 
         [Route("~/makeMove")]
-        public GameState<State, Move> PostMakeMove([FromBody] MakeMoveRequest<Settings, State, Move> req)
+        public virtual GameState<State, Move> PostMakeMove([FromBody] MakeMoveRequest<Settings, State, Move> req)
         {
             return GetFiatGame().MakeMove(req.player, req.settings, req.state, req.move);
         }
 
         [Route("~/isPlayersTurn")]
-        public bool PostIsPlayersTurn([FromBody] MakeMoveRequest<Settings, State, Move> req)
+        public virtual bool PostIsPlayersTurn([FromBody] MakeMoveRequest<Settings, State, Move> req)
         {
             return GetFiatGame().IsPlayersTurn(req.player, req.settings, req.state, req.move);
         }
 
         [Route("~/isMoveValid")]
-        public bool PostIsMoveValid([FromBody] MakeMoveRequest<Settings, State, Move> req)
+        public virtual bool PostIsMoveValid([FromBody] MakeMoveRequest<Settings, State, Move> req)
         {
             return GetFiatGame().IsMoveValid(req.player, req.settings, req.state, req.move);
         }
 
         [Route("~/toClientSettingsAndState")]
-        public Tuple<ClientSettings,GameState<ClientState,Move>> PostToClientSettingsAndState([FromBody] MakeMoveRequest<Settings, State, Move> req)
+        public virtual Tuple<ClientSettings,GameState<ClientState,Move>> PostToClientSettingsAndState([FromBody] MakeMoveRequest<Settings, State, Move> req)
         {
             return GetFiatGame().ToClientSettingsAndState(req.player, req.settings, req.state);
         }
